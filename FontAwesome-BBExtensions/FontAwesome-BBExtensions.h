7be43ab8d1284e90e22d8e21a2c297f50bcd1d1f
William Towe <willbur1984@gmail.com>
2016-11-13 11:10:53 -0500

    Initial Commit

---
+++ FontAwesome-BBExtensions/FontAwesome-BBExtensions.h
@@ -0,0 +1,19 @@
+//
+//  FontAwesome-BBExtensions.h
+//  FontAwesome-BBExtensions
+//
+//  Created by William Towe on 11/13/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontAwesome-BBExtensions.
+FOUNDATION_EXPORT double FontAwesome_BBExtensionsVersionNumber;
+
+//! Project version string for FontAwesome-BBExtensions.
+FOUNDATION_EXPORT const unsigned char FontAwesome_BBExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontAwesome_BBExtensions/PublicHeader.h>
+
+
