aa6ee7fc8a423d8e68135f807f25575988d40691
William Towe <willbur1984@gmail.com>
2016-11-19 13:48:49 -0500

    preprocess info plist to keep version in sync

---
+++ FontAwesome-BBExtensions/FontAwesome-BBExtensions-Info.h
@@ -0,0 +1,14 @@
+//
+//  FontAwesome-BBExtensions-Info.h
+//  FontAwesome-BBExtensions
+//
+//  Created by William Towe on 11/19/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#ifndef FontAwesome_BBExtensions_Info_h
+#define FontAwesome_BBExtensions_Info_h
+
+#define BUNDLE_SHORT_VERSION_STRING 0.0.15
+
+#endif /* FontAwesome_BBExtensions_Info_h */
