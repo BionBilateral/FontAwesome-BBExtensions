ca47a24f84aefebf934d671457f849e6dd3d2fce
William Towe <willbur1984@gmail.com>
2016-11-19 16:13:10 -0500

    added watchKit app and extension

---
+++ watchOS-Demo Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  watchOS-Demo Extension
+//
+//  Created by William Towe on 11/19/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ watchOS-Demo Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  watchOS-Demo Extension
+//
+//  Created by William Towe on 11/19/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
